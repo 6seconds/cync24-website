da8110d5a154377997bb272c8bb7424e25063b9c
Jayesh <93034986+6meowscles@users.noreply.github.com>
2024-07-12 13:01:27 +0530

    asdasd

--- scripts/scrambler.js
+++ scripts/scrambler.js
@@ -33,21 +33,7 @@ function scrambleText(element, targetText) {
       element.classList.add("scrambled"); // Add class to parent container indicating fully scrambled
       
       isScrambling = false; // Reset scrambling flag
-      hasScrambled = true; // Set flag indicating text has fully scrambled
-
-      const preloader = document.querySelector('.preloader');
-      const navbar = document.querySelector('.navbar');
-      const cypher = document.getElementById('cypher-text');
-
-      cypher.style.animationPlayState = 'running';
-      cypher.addEventListener('animationend', () => {
-        preloader.style.animationPlayState = 'running';
-        preloader.addEventListener('animationend', () => {
-          preloader.remove();
-          navbar.style.opacity = '1';
-        });
-        
-      });    
+      hasScrambled = true; // Set flag indicating text has fully scrambled    
     }
 
     iteration++;
@@ -65,65 +51,69 @@ function startContinuousScrambling() {
         .map((letter) => (letter === " " ? " " : letters[Math.floor(Math.random() * letters.length)]))
         .join("");
     }
-  }, 150); // Adjust the interval for continuous scrambling with a slower rate
+  },  100); // Adjust the interval for continuous scrambling with a slower rate
 }
 
-function handleMouseMove(event) {
-  mouseX = event.clientX;
-  mouseY = event.clientY;
-  requestTick();
-}
+// function handleMouseMove(event) {
+//   mouseX = event.clientX;
+//   mouseY = event.clientY;
+//   requestTick();
+// }
 
-function requestTick() {
-  if (!ticking) {
-    requestAnimationFrame(update);
-    ticking = true;
-  }
-}
+// function requestTick() {
+//   if (!ticking) {
+//     requestAnimationFrame(update);
+//     ticking = true;
+//   }
+// }
 
-function update() {
-  ticking = false;
+// function update() {
+//   ticking = false;
 
-  const cursorOutline = document.querySelector(".cursor-outline");
-  const h2Rect = h2Element.getBoundingClientRect();
-  const cursorRect = {
-    left: mouseX - cursorOutline.offsetWidth / 2,
-    right: mouseX + cursorOutline.offsetWidth / 2,
-    top: mouseY - cursorOutline.offsetHeight / 2,
-    bottom: mouseY + cursorOutline.offsetHeight / 2
-  };
+//   const cursorOutline = document.querySelector(".cursor-outline");
+//   const h2Rect = h2Element.getBoundingClientRect();
+//   const cursorRect = {
+//     left: mouseX - cursorOutline.offsetWidth / 2,
+//     right: mouseX + cursorOutline.offsetWidth / 2,
+//     top: mouseY - cursorOutline.offsetHeight / 2,
+//     bottom: mouseY + cursorOutline.offsetHeight / 2
+//   };
 
-  // Check if cursor is over the h2 element
-  if (
-    cursorRect.left >= h2Rect.left &&
-    cursorRect.right <= h2Rect.right &&
-    cursorRect.top >= h2Rect.top &&
-    cursorRect.bottom <= h2Rect.bottom
-  ) {
-    if (!isScrambling && !hasScrambled) {
-      scrambleText(h2Element, targetWord);
-    }
-  }
-}
+//   // Check if cursor is over the h2 element
+//   if (
+//     cursorRect.left >= h2Rect.left &&
+//     cursorRect.right <= h2Rect.right &&
+//     cursorRect.top >= h2Rect.top &&
+//     cursorRect.bottom <= h2Rect.bottom
+//   ) {
+//     if (!isScrambling && !hasScrambled) {
+//       scrambleText(h2Element, targetWord);
+//     }
+//   }
+// }
 
-document.addEventListener("mousemove", handleMouseMove);
+// document.addEventListener("mousemove", handleMouseMove);
 
-if (window.innerWidth >= 768) {
-  document.addEventListener("DOMContentLoaded", () => {
-    startContinuousScrambling();
-  });
-} else {
+document.addEventListener("DOMContentLoaded", () => {
+  startContinuousScrambling();
   const preloader = document.querySelector('.preloader');
   const navbar = document.querySelector('.navbar');
   const cypher = document.getElementById('cypher-text');
+  const C = document.getElementById('C');
+  preloader.style.animationPlayState = 'running';
+  
+  preloader.addEventListener('animationend', () => {
+      
+      scrambleText(h2Element,targetWord);
+      cypher.style.animationPlayState = 'running';
+      cypher.addEventListener('animationstart', () => {
+        scrambleText(h2Element, targetWord);  
+      });
+      cypher.addEventListener('animationend', () => {
+        preloader.remove();
+        navbar.style.opacity = '1';
+      });
 
-  cypher.style.animationPlayState = 'running';
-  cypher.addEventListener('animationend', () => {
-    preloader.style.animationPlayState = 'running';
-    preloader.addEventListener('animationend', () => {
-      preloader.remove();
-      navbar.style.opacity = '1';
-    });
-    
   });  
-}
+  
+});
