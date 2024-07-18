51ebf8a9ff5cec9741d8e0e8e40624dba3c0ba68
Jayesh <93034986+6meowscles@users.noreply.github.com>
2024-07-18 12:28:12 +0530

    Update countdown.js

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -1,5 +1,5 @@
 function updateCountdown() {
-    const targetDate = new Date("2024-08-03T00:00:00");
+    const targetDate = new Date("2024-07-29T00:00:00");
     const now = new Date();
     const timeDifference = targetDate - now;
   
